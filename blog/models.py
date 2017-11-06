181fa45feb5d2eff67b7a256d0bb9653b747db62
wlk <1965880695@qq.com>
2017-11-06 14:17:45 +0800

    add blog app

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,15 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -22,3 +28,6 @@ class Post(models.Model):
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
