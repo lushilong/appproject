dc7b207fdb86e2a5201a9133eb156cada77ea83a
wlk <1965880695@qq.com>
2017-11-06 17:54:34 +0800

    add comments

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@
 #
 import markdown
 from django.shortcuts import render, get_object_or_404
+
+from comments.forms import CommentForm
 from .models import Post, Category
 
 
@@ -20,7 +22,13 @@ def detail(request, pk):
             'markdown.extensions.codehilite',
             'markdown.extensions.toc',
         ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
