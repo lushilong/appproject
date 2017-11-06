36191bd675d8c0d1556c4630584d68b2550d8af1
wlk <1965880695@qq.com>
2017-11-06 15:02:42 +0800

    blog detail

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@
 #
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -30,4 +31,7 @@ class Post(models.Model):
     author = models.ForeignKey(User)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
